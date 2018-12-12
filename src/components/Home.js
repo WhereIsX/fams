538e0d59d1740db8b062f21e5d36bfc2159242d2
Brian DiRito <bdirito@gmail.com>
2018-12-12 14:41:08 -0500

    replaced with actual project

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,25 +12,20 @@ class Home extends Component {
    }
 
     componentDidMount(){
-        if(this.props.user){
-            console.log(this.props.apiUrl, this.props.token)
-            // debugger
-            fetch(`${this.props.apiUrl}/groups`, {
-            headers: {
-                "content-type": "application/json",
-                "accepts": "application/json",
-                Authorization: `${this.props.token}`
-            }
-            })
-            .then(res => res.json())
-            .then(data => {
-            this.setState({ groups: data, filteredGroups: data})
-            console.log("got to home fetch", data)
-            })
-        } else {
-            this.props.history.replace("/")
-        }
-            
+        console.log(this.props.apiUrl, this.props.token)
+        // debugger
+        fetch(`${this.props.apiUrl}/groups`, {
+          headers: {
+            "content-type": "application/json",
+            "accepts": "application/json",
+            Authorization: `${this.props.token}`
+          }
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ groups: data, filteredGroups: data})
+          console.log("got to home fetch", data)
+        })
     }
 
     clickHandler = (e) => {
